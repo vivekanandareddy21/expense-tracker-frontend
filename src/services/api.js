53d3a91f67663688d228701ed0f56da957e90511
vivekanandareddy21 <vivekanandareddy2106@gmail.com>
2026-06-24 21:11:32 +0530

    Production backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://expense-tracker-backend-cxex.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
-
   const token = localStorage.getItem("token");
-
-  console.log("TOKEN =", token);
 
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
